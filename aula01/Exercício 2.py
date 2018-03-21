a26117f73d74d77c72a091e6a880b4f325828fd7
Lucas Airam Castro de Souza <33198508+AiramL@users.noreply.github.com>
2018-03-20 22:42:14 -0300

    Update Exercício 2.py

--- aula01/Exercício 2.py
+++ aula01/Exercício 2.py
@@ -1,20 +1,11 @@
-def montarGradeCurricular(gradeCurricular, codigo, nome, numRequisitos, requisitos):
-    if gradeCurricular != []:
-        for cont in range(len(gradeCurricular)):
-            dicionario = gradeCurricular[cont]
-            dicionario.values
-            if codigo in dict.values(dicionario):
-                return gradeCurricular
-    nova_disciplina = {"codigo": codigo, "nome": nome, "numRequisitos": numRequisitos, "requisitos": requisitos}
-    gradeCurricular+=[nova_disciplina]
-    return gradeCurricular
+from Exercicio01 import montarGradeCurricular
 
 def inserirDisciplina(gradeCurricular):
     nome = raw_input("\nQual o nome da disciplina?\n")
     codigo = raw_input("\nQual o codigo da disciplina?\n")
     numRequisitos = int(raw_input("\nQual o numero de requisitos da disciplina?\n"))
     if numRequisitos != 0:
-        requisito = raw_input("\nQuais s„o os codigos das disciplinas que s„o pre-requisitos?\n")
+        requisito = raw_input("\nQuais s√£o os codigos das disciplinas que s√£o pre-requisitos?\n")
 
     else:
         requisito = []
